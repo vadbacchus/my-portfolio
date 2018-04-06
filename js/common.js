0aa7381c00614e5e8ed21c7224fdb37806792126
vadbacchus <vadgalaiko@gmail.com>
2018-04-06 03:05:58 +0300

    added beautiful menu

--- js/common.js
+++ js/common.js
@@ -1,10 +1,49 @@
 $(function() {
 
-	document.querySelector('.intro__btn').onmousemove = (e) => {
-		const x = e.pageX - e.target.offsetLeft
-		const y = e.pageY - e.target.offsetTop
+const HOVER = document.querySelectorAll('.superhover');
 
-		e.target.style.setProperty('--x', `${ x }px`)
-		e.target.style.setProperty('--y', `${ y }px`)
-	}
+for ( i=0; i < HOVER.length; i++ ) {
+	HOVER[i].onmousemove = (e) => {
+	const x = e.pageX - e.target.offsetLeft;
+	const y = e.pageY - e.target.offsetTop;
+
+	e.target.style.setProperty('--x', `${ x }px`)
+	e.target.style.setProperty('--y', `${ y }px`)
+}
+}
+
+
+
+const MENU = document.querySelector('.nav');
+const CLOSE_BTN = document.querySelectorAll('.nav__closebtn');
+
+function fade(el) {
+  let op = 1;
+  let timer = setInterval(function () {
+    if (op < -1){
+      clearInterval(timer);
+      el.style.display = 'none';
+    }
+    el.style.opacity = op;
+    op -= 0.1;
+  }, 50);
+}
+
+/*	fadeIn --> used CSS animation*/
+
+document.querySelector('.menu__btn').onclick = () => {
+
+	MENU.style.display = 'flex';
+	MENU.style.opacity = 1;
+}
+
+
+for (i=0; i < CLOSE_BTN.length ; i++) {
+
+	CLOSE_BTN[i].onclick = () => {
+
+		fade(MENU);
+
+		}	
+	}	
 });
